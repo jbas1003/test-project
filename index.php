c9396c916a9ca6abf0fa750a62bccf571998d42e
Jon Basco <joncaesar@gmail.com>
2022-11-24 12:27:56 +0800

    First commit.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?
+    require './views/header.php';
+    require './views/side-nav.php';
+    require './views/base.php';
+    require './views/footer.php';
+?>
---
+++ views/footer.php
@@ -0,0 +1,41 @@
+<= "'"; >
+
+<!-- START: Footer-->
+    <footer class="site-footer">
+        2020 &copy; PICK
+    </footer>
+    <!-- END: Footer-->
+
+
+    <!-- START: Back to top-->
+    <a href="#" class="scrollup text-center">
+        <i class="icon-arrow-up"></i>
+    </a>
+    <!-- END: Back to top-->
+
+
+    <!-- START: Template JS-->
+    <script src="dist/vendors/jquery/jquery-3.3.1.min.js"></script>
+    <script src="dist/vendors/jquery-ui/jquery-ui.min.js"></script>
+    <script src="dist/vendors/moment/moment.js"></script>
+    <script src="dist/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="dist/vendors/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- END: Template JS-->
+
+    <!-- START: APP JS-->
+    <script src="dist/js/app.js"></script>
+    <!-- END: APP JS-->
+
+    <!-- START: Page Vendor JS-->
+
+    <!-- END: Page Vendor JS-->
+
+    <!-- START: Page JS-->
+    <script src="dist/js/home.script.js"></script>
+    <!-- END: Page JS-->
+    </body>
+    <!-- END: Body-->
+
+    </html>
+    
+<= "'";
